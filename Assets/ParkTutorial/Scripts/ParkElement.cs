32b06354d18f9101a032f2cac47ced62e6561965
lauri-rustanius <lauri.rustanius@mapbox.com>
2017-11-08 04:21:44 +0200

    initial commit

---
+++ Assets/ParkTutorial/Scripts/ParkElement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.XR.iOS;
+using Mapbox.Unity.Utilities;
+
+public class ParkElement : MonoBehaviour
+{
+	
+	public void Start()
+	{
+
+		var _mask = LayerMask.GetMask("Path");
+		var origin = transform.position;
+		origin.y += 10000f;
+		var ray = new Ray(origin, Vector3.down);
+		RaycastHit hit;
+
+		if (!Physics.Raycast(ray, out hit, Mathf.Infinity, _mask))
+		{
+			Destroy(gameObject);
+		}
+	}
+
+}
---
+++ Assets/ParkTutorial/Scripts/RandomizeAgentDestination.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using Mapbox.Unity.Utilities;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class RandomizeAgentDestination : MonoBehaviour
+{
+	[SerializeField]
+	Vector2 _timeRange;
+
+	[SerializeField]
+	Vector2 _distanceRange;
+
+	NavMeshAgent _agent;
+
+	float _elapsedTime;
+	float _waitTime;
+	bool _isFocused;
+
+	void Awake()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+		SetDestination();
+	}
+
+	void Update()
+	{
+		Debug.DrawLine(transform.position, _agent.destination, Color.red);
+		if (_elapsedTime >= _waitTime || (transform.position - _agent.destination).sqrMagnitude < 1)
+		{
+			_elapsedTime = 0f;
+			_waitTime = Random.Range(_timeRange.x, _timeRange.y);
+			SetDestination();
+		}
+
+		_elapsedTime += Time.deltaTime;
+	}
+
+	void SetDestination()
+	{
+		var target = (Random.insideUnitCircle * Random.Range(_distanceRange.x, _distanceRange.y)).ToVector3xz() + transform.position;
+		if (_agent.isOnNavMesh)
+		{
+			_agent.destination = target;
+		}
+	}
+}
