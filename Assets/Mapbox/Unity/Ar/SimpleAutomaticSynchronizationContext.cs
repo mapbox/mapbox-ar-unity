mapbox/mapbox-ar-unity

commit 41bd3e8a3eba9a6011f36cf440494734f588240f

Compare entire vector since last Alignment against MinimumDeltaDistance. Fixes #16.

Assets/Mapbox/Unity/Ar/SimpleAutomaticSynchronizationContext.cs
@@ -21,12 +21,13 @@
 		List<Vector3> _gpsPositions = new List<Vector3>();
 		List<Vector3> _arNodes = new List<Vector3>();
 
-		float _arDelta;
-		float _gpsDelta;
 		int _count;
 
 		Vector3 _currentArVector;
 		Vector3 _currentAbsoluteGpsVector;
+
+        Vector3 _previousArVector;
+        Vector3 _previousAbsoluteGpsVector;
 
 		/// <summary>
 		/// The synchronization bias.
@@ -76,26 +77,29 @@
 			_count = _arNodes.Count;
 			if (_count > 1)
 			{
-				_currentArVector = _arNodes[_count - 1] - _arNodes[_count - 2];
-				_currentAbsoluteGpsVector = _gpsPositions[_count - 1] - _gpsPositions[_count - 2];
-
-				//TODO: optimize with sqmag?
-				_arDelta += _currentArVector.magnitude;
-				_gpsDelta += _currentAbsoluteGpsVector.magnitude;
+				_currentArVector = arNode - _previousArVector;
+				_currentAbsoluteGpsVector = locationPosition - _previousAbsoluteGpsVector;
 
 				// TODO: try to use ArTrustRange instead!
 				// This would mean no alignment is calculated until the threshold is met.
 				// Perhaps more drift, but also more stable?
-				if (_arDelta < MinimumDeltaDistance || _gpsDelta < MinimumDeltaDistance)
+				if (_currentArVector.magnitude < MinimumDeltaDistance || _currentAbsoluteGpsVector.magnitude < MinimumDeltaDistance)
 				{
-					Unity.Utilities.Console.Instance.Log("Minimum movement not yet met!", "red");
-					return;
+					Unity.Utilities.Console.Instance.Log("Minimum movement not yet met (arDelta: "+ _currentArVector.magnitude + ", gpsDelta: "+ _currentAbsoluteGpsVector.magnitude + ")", "red");
+                    return;
 				}
 
-				_arDelta = 0f;
-				_gpsDelta = 0f;
 				ComputeAlignment();
-			}
+
+                _previousArVector = _currentArVector;
+                _previousAbsoluteGpsVector = _currentAbsoluteGpsVector;
+            }
+            else
+            {
+                //Initialize previous AR / GPS vectors
+                _previousArVector = arNode;
+                _previousAbsoluteGpsVector = locationPosition;
+            }
 		}
 
 		void ComputeAlignment()
@@ -120,8 +124,8 @@
 				bias = Mathf.Clamp01((.5f * (deltaDistance + ArTrustRange - accuracy)) / deltaDistance);
 			}
 
-			// Our new "origin" will be the difference offset between our last nodes (mapped into the same coordinate space).
-			var originOffset = _arNodes[_count - 2] - headingQuaternion * _gpsPositions[_count - 2];
+            // Our new "origin" will be the difference offset between our last nodes (mapped into the same coordinate space).
+            var originOffset = _previousArVector - headingQuaternion * _previousAbsoluteGpsVector;
 
 			// Add the weighted delta.
 			_position = (delta * bias) + originOffset;

commit 58fbdcf54a322cf746624c52ce623ec53c14b51d

Fix "bouncing offset" issue introduced by 41bd3e8a. Fix spaces to tabs.

Assets/Mapbox/Unity/Ar/SimpleAutomaticSynchronizationContext.cs
@@ -26,8 +26,8 @@
 		Vector3 _currentArVector;
 		Vector3 _currentAbsoluteGpsVector;
 
-        Vector3 _previousArVector;
-        Vector3 _previousAbsoluteGpsVector;
+		Vector3 _previousArNode;
+		Vector3 _previousLocationPosition;
 
 		/// <summary>
 		/// The synchronization bias.
@@ -77,8 +77,8 @@
 			_count = _arNodes.Count;
 			if (_count > 1)
 			{
-				_currentArVector = arNode - _previousArVector;
-				_currentAbsoluteGpsVector = locationPosition - _previousAbsoluteGpsVector;
+				_currentArVector = arNode - _previousArNode;
+				_currentAbsoluteGpsVector = locationPosition - _previousLocationPosition;
 
 				// TODO: try to use ArTrustRange instead!
 				// This would mean no alignment is calculated until the threshold is met.
@@ -86,20 +86,21 @@
 				if (_currentArVector.magnitude < MinimumDeltaDistance || _currentAbsoluteGpsVector.magnitude < MinimumDeltaDistance)
 				{
 					Unity.Utilities.Console.Instance.Log("Minimum movement not yet met (arDelta: "+ _currentArVector.magnitude + ", gpsDelta: "+ _currentAbsoluteGpsVector.magnitude + ")", "red");
-                    return;
+					return;
 				}
 
 				ComputeAlignment();
 
-                _previousArVector = _currentArVector;
-                _previousAbsoluteGpsVector = _currentAbsoluteGpsVector;
-            }
-            else
-            {
-                //Initialize previous AR / GPS vectors
-                _previousArVector = arNode;
-                _previousAbsoluteGpsVector = locationPosition;
-            }
+				//Compute next alignment relative to current location.
+				_previousArNode = arNode;
+				_previousLocationPosition = locationPosition;
+			}
+			else
+			{
+				//Initialize previous AR / GPS vectors
+				_previousArNode = arNode;
+				_previousLocationPosition = locationPosition;
+			}
 		}
 
 		void ComputeAlignment()
@@ -124,8 +125,8 @@
 				bias = Mathf.Clamp01((.5f * (deltaDistance + ArTrustRange - accuracy)) / deltaDistance);
 			}
 
-            // Our new "origin" will be the difference offset between our last nodes (mapped into the same coordinate space).
-            var originOffset = _previousArVector - headingQuaternion * _previousAbsoluteGpsVector;
+			// Our new "origin" will be the difference offset between our last nodes (mapped into the same coordinate space).
+			var originOffset = _previousArNode - headingQuaternion * _previousLocationPosition;
 
 			// Add the weighted delta.
 			_position = (delta * bias) + originOffset;