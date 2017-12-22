6230980a99f2bd8c61b105b27be7f757f35f47f9
lauri-rustanius <lauri.rustanius@mapbox.com>
2017-12-22 23:50:43 +0200

    ARTabletop setup

---
+++ Assets/ARTabletop/Scripts/ARPlaneHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace UnityARInterface
+{
+    public class ARPlaneHandler : MonoBehaviour
+    {
+        public static Action resetARPlane;
+        public static Action<BoundedPlane> returnARPlane;
+        private string _planeId;
+        private BoundedPlane _cachedARPlane;
+
+        void Start()
+        {
+            ARInterface.planeAdded += UpdateARPlane;
+            ARInterface.planeUpdated += UpdateARPlane;
+        }
+
+
+        void UpdateARPlane(BoundedPlane arPlane)
+        {
+            
+            if(_planeId == null)
+            {
+                _planeId = arPlane.id;
+            }
+
+            if(arPlane.id == _planeId) 
+            {
+                _cachedARPlane = arPlane;
+            }
+
+            returnARPlane(_cachedARPlane);
+
+        }
+    }
+}
---
+++ Assets/ARTabletop/Scripts/PlaceMapOnARPlane.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityARInterface;
+
+public class PlaceMapOnARPlane : MonoBehaviour {
+
+    [SerializeField]
+    Transform _mapTransform;
+
+	// Use this for initialization
+	void Start () 
+    {
+        ARPlaneHandler.returnARPlane += PlaceMap;
+        ARPlaneHandler.resetARPlane += ResetPlane;
+	}
+
+    void PlaceMap(BoundedPlane plane)
+    {
+        if(!_mapTransform.gameObject.activeSelf)
+        {
+            _mapTransform.gameObject.SetActive(true);
+        }
+        _mapTransform.position = plane.center;
+    }
+
+    void ResetPlane()
+    {
+        _mapTransform.gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        ARPlaneHandler.returnARPlane -= PlaceMap;
+    }
+
+}
