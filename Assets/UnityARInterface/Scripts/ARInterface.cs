a323cb356944d8d7fad2e78b8710d0e7f9032eea
BergWerkGIS <wb@BergWerk-GIS.at>
2018-01-19 15:33:40 +0100

    update to latest UnityARInterface

--- Assets/UnityARInterface/Scripts/ARInterface.cs
+++ Assets/UnityARInterface/Scripts/ARInterface.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -45,7 +46,11 @@ namespace UnityARInterface
         public static Action<BoundedPlane> planeUpdated;
         public static Action<BoundedPlane> planeRemoved;
 
-        public abstract bool StartService(Settings settings);
+        public virtual bool IsRunning { get; protected set; } 
+
+        public virtual bool IsSupported { get { return true; } }
+
+        public abstract IEnumerator StartService(Settings settings);
 
         public abstract void StopService();
 
